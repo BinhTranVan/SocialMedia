da94139f1cdc828ca0fe763236d941cf7b7559d6
BinhTranVan <binhtranvan31@gmail.com>
2024-09-22 12:49:04 +0700

    1-deleting-posts

---
+++ src/lib/ky.ts
@@ -0,0 +1,12 @@
+import ky from "ky";
+
+const kyInstance = ky.create({
+  parseJson: (text) =>
+    JSON.parse(text, (key, value) => {
+      if (key.endsWith("At")) return new Date(value);
+      console.log("value: ", value);
+      return value;
+    }),
+});
+
+export default kyInstance;
